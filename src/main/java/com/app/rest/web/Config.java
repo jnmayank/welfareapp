5aca63a055705cc819af66c0a85ed22c0a6548c2
nitesh <niteshbisht98@gmail.com>
2016-12-16 08:59:45 +0530

    added changes

---
+++ src/main/java/com/app/rest/web/Config.java
@@ -0,0 +1,10 @@
+package com.app.rest.web;
+/**
+Time   : 10:43:40 pm
+created: 10-Dec-2016
+author : nitesh
+**/
+
+public class Config {
+
+}
--- src/main/webapp/resources/scripts/js/controllers/homecontroller.js
+++ src/main/webapp/resources/scripts/js/controllers/homecontroller.js
@@ -3,5 +3,17 @@
  */
 
 app.controller('HomeCtrl', function($scope) {
-	//$scope.user = authentication.user.name;
+
+	$scope.$on('$viewContentLoaded', function() {
+		$scope.style = document.createElement('link');
+		$scope.style.type = 'text/css';
+		$scope.style.href = 'resources/css/style.css';
+		$scope.style.rel = 'stylesheet';
+		$scope.style = document.head.appendChild($scope.style);
+	});
+
+	$scope.$on('$destroy', function() {
+		$scope._style.parentNode.removeChild($scope.style);
+		delete $scope._style;
+	});
 });
